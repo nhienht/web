55e0b1cc02608cd25e5e580bd7f1d9e1f35ece1c
nhieht <NhienHTCE140122@fpt.edu.vn>
2020-10-16 16:58:14 +0700

    mysql

--- WebStory/WebStory/DBConnection/DBConnection.cs
+++ WebStory/WebStory/DBConnection/DBConnection.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using System.Configuration;
+using MySql.Data.MySqlClient;
+
 namespace WebStory.DBConnection
 {
     public class DBConnection
     {
-        public SqlConnection conn { get; set; }
+        public MySqlConnection conn { get; set; }
 
         public DBConnection()
         {
             string mainconn = ConfigurationManager.ConnectionStrings["PRN"].ConnectionString;
 
-            this.conn = new SqlConnection(mainconn);
+            this.conn = new MySqlConnection(mainconn);
         }
     }
 }
