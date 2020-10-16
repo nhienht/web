25875841461df48cba9bafaae1590647aaa59599
nhieht <NhienHTCE140122@fpt.edu.vn>
2020-10-16 17:36:35 +0700

    hi

---
+++ WebStory/WebStory/DAO/ChapterDao.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebStory.Models;
+
+namespace WebStory.DAO
+{
+
+    public class ChapterDao
+    {
+        private MySqlConnection conn { get; set; }
+        public ChapterDao()
+        {
+            this.conn = new DBConnection.DBConnection().conn;
+        }
+        public int insertChapter(Chapter c)
+        {
+            if(c.getIdTruyen() <= 1)
+            {
+                
+            }
+
+            return -1;
+        }
+    }
+}
